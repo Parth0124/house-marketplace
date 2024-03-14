b78f845d13ca9097e764582a01c915d423a05523
Abhiraj412 <abhangparth@gmail.com>
2024-03-14 15:22:28 +0530

    navbar-ommit3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,29 +16,29 @@ function Navbar() {
   return (
     <footer className='navbar'>
       <nav className='navbarNav'>
-        <ul className='navbarListItems'>
-          <li className='navbarListItem' onClick={() => navigate("/")}>
+        <span className='navbarListItems'>
+          <span className='navbarListItem' onClick={() => navigate("/")}>
             <ExploreIcon fill={pathMatchRoute("/") ? "#2c2c2c" : "#8f8f8f"} width="36px" height="36px"/>
             <p className={ pathMatchRoute("/") ? "navbarListItemNameActive" : "navbarListItemName"}>
               Explore
             </p>
-          </li>
-          <li className='navbarListItem' onClick={() => navigate("/offers")}>
+          </span>
+          <span className='navbarListItem' onClick={() => navigate("/offers")}>
             <OfferIcon fill={pathMatchRoute("/offers") ? "#2c2c2c" : "#8f8f8f"} width="36px" height="36px"/>
             <p className={ pathMatchRoute("/offers") ? "navbarListItemNameActive" : "navbarListItemName" }>
               Offers
             </p>
-          </li>
-          <li className='navbarListItem' onClick={() => navigate("/profile")}>
+          </span>
+          <span className='navbarListItem' onClick={() => navigate("/profile")}>
             <PersonOutlineIcon fill={pathMatchRoute("/profile") ? "#2c2c2c" : "#8f8f8f"} width="36px" height="36px"/>
             <p className={ pathMatchRoute("/profile") ? "navbarListItemNameActive" : "navbarListItemName" }>
               Profile
             </p>
-          </li>
-        </ul>
+          </span>
+        </span>
       </nav>
     </footer>
   );
 }
 
-export default Navbar
+export default Navbar;
