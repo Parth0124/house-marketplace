0b9396404ccaa4f487647974fd8bca45b1028985
Abhiraj412 <abhangparth@gmail.com>
2024-03-15 16:23:35 +0530

    register-user

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -13,11 +13,51 @@ function SignIn() {
 
   const {email,password}= formData
 
+  const navigate=useNavigate()
+
+  const onChange = (event) => {
+    setFormData((prevState)=>({
+      ...prevState,
+      [event.target.id]: event.target.value
+    }))
+  } 
+ 
+   
+
   return (
-    <div>
-      <h1>Sign In</h1>
-    </div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!!</p>
+        </header>
+
+        <form>
+          <input type="email " className="emailInput" placeholder="Email" id='email' value={email} onChange={onChange} />
+          <div className="passwordInputDiv">
+            <input type={showPassword ? "text" : 'password'} className="passwordInput" placeholder="Password" id='password' value={password} onChange={onChange }/>
+            <img src={visibilityIcon} alt="show password" className="showPassword" onClick={()=> setShowPassword((prevState)=>!prevState)} />
+          </div>
+          
+          <Link to='./forgot-password' className="forgotPasswordLink">
+            Forgot Password  
+          </Link>
+
+          <div className="signInBar">
+             <p className="signInText">
+              Sign In
+             </p>
+             <button className="signInButton">
+              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+             </button>
+          </div>
+        </form>
+        {/* {Google OAuth} */}
+         
+
+         <Link to='/sign-up' className=" registerLink">Sign Up Instead</Link>
+      </div> 
+    </>
   )
 }
 
-export default SignIn
+export default SignIn 
