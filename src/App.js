83cc1a0106aff60b4e953f83237c2785e118a43a
Abhiraj412 <abhangparth@gmail.com>
2024-03-17 18:42:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@
  import Profile from './pages/Profile';
  import SignIn from './pages/SignIn';
  import SignUp from './pages/SignUp';
-
+ import { ToastContainer } from 'react-toastify';
+ import 'react-toastify/dist/ReactToastify.css';
  
  function App() {
   return(
@@ -22,6 +23,7 @@
         </Routes>
           <Navbar/>
       </Router>
+      <ToastContainer />
     </>
   )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,6 +5,7 @@ import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 import {getAuth, createUserWithEmailAndPassword,  updateProfile} from 'firebase/auth'
 import {db} from '../firebase.config'
 import { setDoc, serverTimestamp, doc  } from "firebase/firestore"
+import {toast} from 'react-toastify'
 
 function SignUp() {
 
@@ -47,7 +48,7 @@ function SignUp() {
 
         navigate('/ ')
     }catch(error) {
-        console.log(error);
+        toast.error('Something went wrong with the sign up.')
     }
   }
   
