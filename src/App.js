d067aef5603c8b4ac42de7411705ecc707e064f5
Abhiraj412 <abhangparth@gmail.com>
2024-03-18 23:41:36 +0530

    explore page start but no data yet displyed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@
  import { ToastContainer } from 'react-toastify';
  import 'react-toastify/dist/ReactToastify.css';
  import PrivateRoute from './components/PrivateRoute';
+ import Category from './pages/Category';
  
  function App() {
   return(
@@ -17,6 +18,7 @@
         <Routes>
           <Route path='/' element={<Explore/>}/>
           <Route path='/offers' element={<Offers/>}/>
+          <Route path='/category/:categoryName' element={<Category/>}/>
           <Route path='/profile' element={<PrivateRoute/>}>
             <Route path='/profile' element={<Profile/>}/>
           </Route>
