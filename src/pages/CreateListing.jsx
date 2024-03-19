15372fec32fe53d8efb73dafff8df81050bd3c08
Abhiraj412 <abhangparth@gmail.com>
2024-03-20 03:27:04 +0530

    listing aded to database

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -14,7 +14,8 @@ import { v4 as uuidv4 } from 'uuid'
 import Spinner from '../components/Spinner'
 
 function CreateListing() {
-//   const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+  // eslint-disable-next-line
+  const [geolocationEnabled, setGeolocationEnabled] = useState(false)
   const [loading, setLoading] = useState(false)
   const [formData, setFormData] = useState({
     type: 'rent',
@@ -66,6 +67,7 @@ function CreateListing() {
     return () => {
       isMounted.current = false
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMounted])
 
   const onSubmit = async (e) => {
@@ -85,34 +87,35 @@ function CreateListing() {
       return
     }
 
-    // let geolocation = {}
-    // let location
+    let geolocation = {}
+    let location
 
-    // if (geolocationEnabled) {
-    //   const response = await fetch(
-    //     `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
-    //   )
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      )
 
-    //   const data = await response.json()
+      const data = await response.json()
 
-    //   geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
-    //   geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
 
-    //   location =
-    //     data.status === 'ZERO_RESULTS'
-    //       ? undefined
-    //       : data.results[0]?.formatted_address
+      location =
+        data.status === 'ZERO_RESULTS'
+          ? undefined
+          : data.results[0]?.formatted_address
 
-    //   if (location === undefined || location.includes('undefined')) {
-    //     setLoading(false)
-    //     toast.error('Please enter a correct address')
-    //     return
-    //   }
-    // } else {
-    //   geolocation.lat = latitude
-    //   geolocation.lng = longitude
-    // }
+      if (location === undefined || location.includes('undefined')) {
+        setLoading(false)
+        toast.error('Please enter a correct address')
+        return
+      }
+    } else {
+      geolocation.lat = latitude
+      geolocation.lng = longitude
+    }
 
+    // Store image in firebase
     const storeImage = async (image) => {
       return new Promise((resolve, reject) => {
         const storage = getStorage()
@@ -143,6 +146,8 @@ function CreateListing() {
             reject(error)
           },
           () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
               resolve(downloadURL)
             })
@@ -151,7 +156,7 @@ function CreateListing() {
       })
     }
 
-    const imgUrls = await Promise.all(
+    const imageUrls = await Promise.all(
       [...images].map((image) => storeImage(image))
     ).catch(() => {
       setLoading(false)
@@ -161,17 +166,17 @@ function CreateListing() {
 
     const formDataCopy = {
       ...formData,
-      imgUrls,
-    //   geolocation,
+      imageUrls,
+      geolocation,
       timestamp: serverTimestamp(),
     }
 
-    // formDataCopy.location = address
+    formDataCopy.location = address
     delete formDataCopy.images
     delete formDataCopy.address
     !formDataCopy.offer && delete formDataCopy.discountedPrice
 
-    const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+    const docRef = await addDoc(collection(db, 'listing'), formDataCopy)
     setLoading(false)
     toast.success('Listing saved')
     navigate(`/category/${formDataCopy.type}/${docRef.id}`)
@@ -187,7 +192,7 @@ function CreateListing() {
       boolean = false
     }
 
-    
+    // Files
     if (e.target.files) {
       setFormData((prevState) => ({
         ...prevState,
@@ -195,7 +200,7 @@ function CreateListing() {
       }))
     }
 
-
+    // Text/Booleans/Numbers
     if (!e.target.files) {
       setFormData((prevState) => ({
         ...prevState,
